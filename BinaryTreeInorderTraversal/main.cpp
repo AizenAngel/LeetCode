7e02ccfa3ee58442559cf216110f5e60c82cc696
AizenAngel <boza.mitrovic@gmail.com>
2021-04-15 16:09:52 +0200

    Add BinaryTreeInorderTraversial

---
+++ BinaryTreeInorderTraversal/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    
+    void helper(vector<int>& vec, TreeNode* root){
+        if (root == nullptr)
+            return;
+        
+        helper(vec, root->left);
+        vec.push_back(root->val);
+        helper(vec, root->right);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {        
+        vector<int> res;
+        helper(res, root);
+        return res;
+    }
+};
